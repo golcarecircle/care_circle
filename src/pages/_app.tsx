aedb55376826bb5fab40224fc4c1c9ccf1be31f0
OliverMengich <kemeioliver2000@gmail.com>
2023-03-20 10:00:29 +0300

    Global styles update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import Layoutcomponent from '@/components/layout/Layout.component';
+import { ReactElement, ReactNode } from 'react';
+import { NextPage } from 'next';
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  return (
+    <Layoutcomponent>
+      <Component {...pageProps} />
+    </Layoutcomponent>
+  )
 }
