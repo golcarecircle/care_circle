6f6edc0d304f047b7fad6f17cdb75d1833a6095c
OliverMengich <kemeioliver2000@gmail.com>
2023-03-28 13:16:19 +0300

    Switches controllers fetch

--- src/pages/api/auth/sign-up.ts
+++ src/pages/api/auth/sign-up.ts
@@ -1,29 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import clientPromise from './clientPromise';
+import { createUser } from '../controllers/user.controller';
 type Data = {
   message: string
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-//   res.status(200).json({ name: 'John Doe' })
     if (req.method==='POST') {
         const {email, password, ...otherProps} = req.body;
         if (!email || !email.includes('@') || !password) {
             res.status(422).json({ message: 'Invalid Data' });
             return;
         }
-        const db = (await clientPromise).db()
-        const userExists = await db.collection('users').findOne({email})
-        if (userExists) {
+        const data = await createUser({email, password, ...otherProps})
+        if (!data) {
             res.status(422).json({ message: 'User already exists' });
             (await clientPromise).close()
             return;
         }
-        const status = await db.collection('users').insertOne({email,password,...otherProps})
-        res.status(201).json({ message: 'User created', ...status });
-        //Close DB connection
-        (await clientPromise).close()
+        // sign in the user
+        res.status(201).json({ message: 'User created',...data });
         return
     }else{
         res.status(500).send({message:'Invalid Route'})
