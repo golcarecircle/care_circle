7041d11b8160cea383fbb5c344a825f0a9f45a7e
OliverMengich <kemeioliver2000@gmail.com>
2023-03-28 13:43:21 +0300

    Removes typo on form component

--- src/components/Form/Form.component.tsx
+++ src/components/Form/Form.component.tsx
@@ -80,7 +80,7 @@ function FormComponent() {
             [next.name]: next.value
         }
     }
-    const [state,dispatch] = useReducer(signInReducergit,formVals)
+    const [state,dispatch] = useReducer(signInReducer,formVals)
     function handleOnChange(e: React.FormEvent<HTMLInputElement> | React.FormEvent<HTMLSelectElement>){
         if (e.target instanceof HTMLSelectElement) {
             dispatch({name: e.target.name,value: e.target.value})
