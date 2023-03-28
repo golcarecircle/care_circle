05f9d145cdec4382298eb174ce8a4d8da1085b2c
OliverMengich <kemeioliver2000@gmail.com>
2023-03-28 13:17:38 +0300

    Type assertions for user cntroller

--- src/pages/api/controllers/user.controller.ts
+++ src/pages/api/controllers/user.controller.ts
@@ -1,9 +1,28 @@
 import UserModel from "../models/user.model"
+import bcryptjs from 'bcryptjs'
+import { User } from "../users";
 export const getAllUser =async () => {
     const users = await UserModel.find({})
     return users;
 }
-export const getUserById = async (id: string) => {
-    const user = await UserModel.findById(id);
+export const getUserById = async (email: string) => {
+    const user: User | null = await UserModel.findOne({
+        email
+    });
+    if(!user) throw new Error("No User Found");
     return user;
+}
+export const createUser = async (user: User) => {
+    const userAdded:User = {
+        ...user,
+        password: bcryptjs.hashSync(user.password, 10),
+        userType: 'PATIENT',
+    }
+    console.log('Request to add user: ', userAdded);
+    const finduser = await UserModel.findOne({
+        email: userAdded.email
+    });
+    if(finduser) throw new Error("No User Found");
+    const newUser = await UserModel.create(userAdded);
+    return newUser;
 }
