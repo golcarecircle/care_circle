c3e11dea5cd3ab1a2ee2b16b33e22c2117c33263
OliverMengich <kemeioliver2000@gmail.com>
2023-03-28 13:18:39 +0300

    Dynamic navigation bar

--- src/components/NavBar/Navbar.component.tsx
+++ src/components/NavBar/Navbar.component.tsx
@@ -2,7 +2,11 @@ import Link from "next/link";
 import React from "react";
 import navStyles from './Navbar.module.css'
 import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { signOut, signIn } from "next-auth/react";
 function NavComponent() {
+    const {data: session} = useSession();
+
     return(
         <header className={navStyles.header}>
             <h1>CareCircle</h1>
@@ -17,25 +21,48 @@ function NavComponent() {
                     <li>
                         <Link href='/partners'>Partners</Link>
                     </li>
-                    <li>
-                        <Link href='/my-dashboard'>My Dashboard</Link>
-                    </li>
+                    {
+                        session && (
+                            <li>
+                                <Link href='/my-dashboard'>My Dashboard</Link>
+                            </li>
+                        )
+                    }
                     <li>
                         <Link href='/contact'>Contact Us</Link>
                     </li>
                 </ul>
                 <div style={{display: 'flex', alignItems:'center'}}>
-                    <button className={navStyles.btn}>Book Appointment</button>
-                    <button className={navStyles.btn}>
-                        <Link href='/auth'>Login/Register</Link>
-                    </button>
-                    <div style={{border:'1px solid #ccc', display:'flex', padding: '5px', borderRadius: '30px'}}>
-                        <Image src='/images/cart.webp' alt="image" style={{borderRadius:'50%', cursor:'pointer', margin:'0 10px'}} height={40} width={40}/>
-                        <div>
-                            <h4>Oliver Kipkemei</h4>
-                            <p>+254741954426</p>
-                        </div>
-                    </div>
+                    {
+                        session && (
+                            <div style={{border:'1px solid #ccc', display:'flex', padding: '5px', borderRadius: '30px'}}>
+                                <Image src='/images/cart.webp' alt="image" style={{borderRadius:'50%', cursor:'pointer', margin:'0 10px'}} height={40} width={40}/>
+                                <div>
+                                    <h4>Oliver Kipkemei</h4>
+                                    <p>+254741954426</p>
+                                </div>
+                            </div>
+                        )
+                    }
+                    {
+                        !session && (
+                            <button className={navStyles.btn}>
+                                <Link href='/auth'>Login/Register</Link>
+                            </button>
+                        )
+                    }
+                    {
+                        session && (
+                            <button onClick={()=>signOut()} className={navStyles.btn}>
+                                Logout
+                            </button>
+                        )
+                    }
+                    {
+                        session && (
+                            <button className={navStyles.btn}>Book Appointment</button>
+                        )
+                    }
                 </div>
             </nav>
         </header>
