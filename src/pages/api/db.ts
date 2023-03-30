a9f8c0bd587f063e6e48946f9244f7af9b4a7613
OliverMengich <kemeioliver2000@gmail.com>
2023-03-30 11:06:40 +0300

    Separates database auth

---
+++ src/pages/api/db.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+//connect to mongodb
+const connectionUrl = process.env.MONGODB_URI || "mongodb://localhost:27017/care_circle";
+const connectDB = async () => {
+    console.log('Environment URI is: ', process.env.MONGODB_URI);
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI||connectionUrl);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (err) {
+        console.error('Error Encountered: ',err);
+        process.exit(1);
+    }
+};
+export default connectDB;
