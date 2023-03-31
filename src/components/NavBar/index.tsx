96c4440e05d4571f845d1d83a7b967dadddb2ac9
michaelmuriith <mikemuriith@gmail.com>
2023-03-31 16:14:31 +0300

    hero

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,30 +1,51 @@
-import React from "react";
-import {medIcon} from "@/assets/icons";
+import React, { useState, useEffect } from "react";
+import { medIcon } from "@/assets/icons";
 import styles from "./NavBar.module.css";
 import Link from "next/link";
 import Button from "../Btn";
 
 export default function NavBar() {
-    return (
-        <nav className = {styles.nav}>
-            <div className={styles.logo}>
-                {medIcon()}
-                <h1 className={styles.brand}> CareCircle</h1>
-            </div>
-            <ul className={styles.links}>
-                <li>
-                    <Link href={"/"}>Home</Link>
-                </li>
-                <li>
-                    <Link href={"/about"}>About</Link>
-                </li>
-                <li>
-                    <Link href={"/how-to-use"}>How to Use</Link>
-                </li>
-                <li>
-                    <Button onClick={() => { } } name={"Sign Up"} />
-                </li>
-            </ul>
-        </nav>
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+
+    setVisible(
+      (prevScrollPos > currentScrollPos && prevScrollPos - currentScrollPos > 70) ||
+        currentScrollPos < 10
     );
+
+    setPrevScrollPos(currentScrollPos);
+  };
+
+  return (
+    <nav style={{ backgroundColor: visible ? "#f8f8f8" : "transparent" }}>
+      <div className={styles.nav}>
+        <div className={styles.logo}>
+          {medIcon()}
+          <h1 className={styles.brand}> CareCircle</h1>
+        </div>
+        <ul className={styles.links}>
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/about"}>About</Link>
+          </li>
+          <li>
+            <Link href={"/how-to-use"}>How to Use</Link>
+          </li>
+          <li>
+            <Button onClick={() => {}} name={"Sign Up"} />
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
--- src/pages/ui/LandingPage/components/HeroSection/Index.tsx
+++ src/pages/ui/LandingPage/components/HeroSection/Index.tsx
@@ -1,11 +1,23 @@
 import { FC } from 'react'
+import styles from './HeroSection.module.css'
+import {heartIcon} from '@/assets/icons'
+
 
 interface HeroSectionProps {
   
 }
 
 const HeroSection: FC<HeroSectionProps> = ({}) => {
-  return <div>HeroSection</div>
+  return <section className={styles.section}>
+    <div className={styles.container}>
+      <div className={styles.contRight}></div>
+      <div className={styles.contLeft}>
+        {heartIcon()}
+        <h1 className={styles.title}>Join Care Circle </h1>
+        <p className={styles.subtitle}>Take control of your health with cutting-edge technology at your fingertips.</p>
+      </div>
+    </div>
+  </section>
 }
 
 export default HeroSection
