e34b5725d4507b0d2f904ef3c5d3875ebf6fa21f
OliverMengich <kemeioliver2000@gmail.com>
2023-04-05 13:20:34 +0300

    Adds middleware and patient search reducer

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,12 +1,16 @@
 import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit";
 import recordsReducer from './patientInfoSlice';
 import appointmentsReducer from './doctorSlice';
-import { createWrapper } from 'next-redux-wrapper'
+import { createWrapper } from 'next-redux-wrapper';
+import { patientSearchApi } from "./patientSearch";
 export const store =()=>( configureStore({
     reducer: {
         recordsReducer,
-        appointmentsReducer
-    }
+        appointmentsReducer,
+		[patientSearchApi.reducerPath]: patientSearchApi.reducer,
+    },
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(patientSearchApi.middleware),
 }));
 export type AppStore = ReturnType<typeof store>
 export type AppState = ReturnType<AppStore['getState']>
