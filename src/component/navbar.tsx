5f81d5d59e6c9a87a61d727fe683dec6a1ca3a1c
OliverMengich <kemeioliver2000@gmail.com>
2023-04-26 20:02:17 +0300

    Adds item list component to display health tips

--- src/component/navbar.tsx
+++ src/component/navbar.tsx
@@ -72,6 +72,15 @@ const Navbar: FC<NavbarProps> = ({}) => {
             </li>
             <li>
               <a
+                href="/health-tips"
+                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded lg:bg-transparent lg:text-blue-700 lg:p-0 dark:text-white"
+                aria-current="page"
+              >
+                Daily Feeds
+              </a>
+            </li>
+            <li>
+              <a
                 href="#"
                 className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
               >
